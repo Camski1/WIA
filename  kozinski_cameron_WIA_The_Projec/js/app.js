289eb663ca405bb2d48cae2ae241ab46e65bc845
Cameron Kozinski <camski20@gmail.com>
2014-08-25 20:16:32 -0400

    no message

---  kozinski_cameron_WIA_The_Projec/js/app.js
+++  kozinski_cameron_WIA_The_Projec/js/app.js
@@ -1,21 +1,3 @@
-/*
-$( "#btn1" ).click(function() {
-  $( "#popUp" ).show( "slow", function() {
-  });
-});
-
-$( ".cancel" ).click(function() {
-  $( "#popUp" ).hide( "slow", function() {
-  });
-});
-
-$( ".submit" ).click(function() {
-  $( "#popUp" ).hide( "slow", function() {
-  });
-});
-
-
-*/
 
 $(function(){
   $('#world-map').vectorMap({
@@ -27,13 +9,12 @@ $(function(){
     hoverColor: false,
     markerStyle: {
       initial: {
-        fill: '#B1D873',
-        stroke: '#383f47'
+      fill: '#B1D873',
+      stroke: '#383f47'
       }
     },
     backgroundColor: '#6C597F',
     markers: [
-
       {latLng: [46.02, 33.55], name: 'Crimea'},
       {latLng: [1.3, 103.8], name: 'Singapore'},
       {latLng: [31.3, -99.38], name: 'Dallas'},
@@ -44,17 +25,33 @@ $(function(){
   });
 });
 
-
-
-
-
-$("#btn1, .submit, .cancel").click(function () {
+$("#btn1, #PopB2, #PopB1").click(function () {
   if ( $( "#popUp" ).is( ":hidden" ) ) {
-    $( "#popUp, .overlay" ).show("slow");
+    $( "#popUp" ).show("slow");
   }else{
-  	 $( "#popUp" ).hide("slow");
+    $( "#popUp" ).hide("slow");
   }
 });
 
+$('#btn2').on('click', function(e){
+  e.preventDefault();
+  window.location.assign('#letter');
+});
 
+window.onload = function() {
+  if (Modernizr.canvas) {
+     var Canvas1 = document.getElementById('Canvas1');
+        var ctx1 = Canvas1.getContext("2d");
 
+        var textStr = "Created by: Cameron Kozinski";
+
+        
+        ctx1.font = "16pt";
+        ctx1.fillStyle = "black";
+        ctx1.strokeStyle = "black";
+        ctx1.fillText(textStr, 30, 15);
+        ctx1.strokeText(textStr, 30, 15); 
+
+     
+  };//end Modernizr (if)
+}// end onload function
